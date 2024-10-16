eeb4947aef56946741cf595a45b3ce9da499e692
David da Rocha Fernandes <davidrfernandes03@gmail.com>
2024-10-16 17:55:55 +0100

    Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment mathA1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathA1.GetSummary());
+        Console.WriteLine(mathA1.GetHomeworkList());
+
+        WritingAssignment writA1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writA1.GetSummary());
+        Console.WriteLine(writA1.GetWritingInformation());
     }
 }
