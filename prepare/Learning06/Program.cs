9d3c5ec9b575b60f382ea9bc42603de0d4a213d5
David da Rocha Fernandes <davidrfernandes03@gmail.com>
2024-10-23 16:14:03 +0100

    Learning06

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        // New square object
+        Square square = new Square("Blue", 2.5);
+
+        // New rectangle object
+        Rectangle rectangle = new Rectangle("Red", 5, 4);
+
+        // New circle object
+        Circle circle = new Circle("Yellow", 3.2);
+
+        // A list with all objects
+        List<Shape> shapeList = new List<Shape>();
+        shapeList.Add(square);
+        shapeList.Add(rectangle);
+        shapeList.Add(circle);
+
+        foreach (Shape shapes in shapeList)
+        {
+            Console.WriteLine(shapes.GetColor());
+            Console.WriteLine(shapes.GetArea());
+            Console.WriteLine();
+        }
     }
 }
